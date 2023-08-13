0c3b597020c78138cf5b99598d9f6680fc372f5a
mehraasahil <mehra3249@gmail.com>
2023-08-14 03:35:07 +0530

    added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.post('/register',verifyToken ,async(req,resp)=>{
+app.post('/register' ,async(req,resp)=>{
   const data = new User(req.body);
   let result = await data.save();
   result = result.toObject();
@@ -28,7 +28,7 @@ app.post('/register',verifyToken ,async(req,resp)=>{
 
 
 
-app.post('/login',verifyToken,async (req,resp)=>{
+app.post('/login',async (req,resp)=>{
   if(req.body.password && req.body.email)
   {
     let user = await User.findOne(req.body).select("-password");
